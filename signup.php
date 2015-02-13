5060c01dff5c8edb9c75834267fc2248cf3475b5
Ásgeir Ástvaldsson <asgeiras@users.noreply.github.com>
2015-02-13 15:09:19 +0100

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,6 +1,6 @@
 <?php
 error_reporting(~0);
-ini_set('display_errors', 1);
+ini_set('display_errors', 0);
 //session_start();
 //if (!(isset($_SESSION['login']) && $_SESSION['login'] != '')) {
 	//header ("Location: login.php");
