9c0f59268d2c79fd4e0a678c5af4125dd0d84d2c
Per Enström <per@perenstrom.com>
2015-02-26 16:03:25 +0100

    Started work on search code
    Cleaned up code, removed some jacvascript

--- search.php
+++ search.php
@@ -46,50 +46,76 @@ if($_GET['type'] == 'word' || !isset($_GET['type'])) {
 
 	<table width="20" border="0">
 		<tr>
-			<td colspan=2>Include:<br><input type="text" title="Tip: use % as wildcard" name="term1" value="<?php echo $_SESSION['term1']; ?>" onKeyPress="if (event.keyCode == 13) pageforward(1);"/></td>
-			<td><br><input type="text" title="Tip: use % as wildcard" name="term2" value="<?php echo $_SESSION['term2']; ?>" onKeyPress="if (event.keyCode == 13) pageforward(1);"/></td>
-			<td><br><input type="text" title="Tip: use % as wildcard" name="term3" value="<?php echo $_SESSION['term3']; ?>" onKeyPress="if (event.keyCode == 13) pageforward(1);"/></td>
-			<td><br><input type="text" title="Tip: use % as wildcard" name="term4" value="<?php echo $_SESSION['term4']; ?>" onKeyPress="if (event.keyCode == 13) pageforward(1);"/></td>
+			<td colspan="4">Include:</td>
+		</tr>
+		<tr>
+			<td><input type="text" title="Tip: use % as wildcard" name="term1" value="<?php echo $_SESSION['term1']; ?>" /></td>
+			<td><input type="text" title="Tip: use % as wildcard" name="term2" value="<?php echo $_SESSION['term2']; ?>" /></td>
+			<td><input type="text" title="Tip: use % as wildcard" name="term3" value="<?php echo $_SESSION['term3']; ?>" /></td>
+			<td><input type="text" title="Tip: use % as wildcard" name="term4" value="<?php echo $_SESSION['term4']; ?>" /></td>
 		</tr>
 
 		<tr>
-			<td colspan=2>Exclude:<br><input type="text" title="Tip: use % as wildcard" name="notterm1" value="<?php echo $_SESSION['notterm1']; ?>" onKeyPress="if (event.keyCode == 13) pageforward(1);"/></td>
-			<td><br><input type="text" title="Tip: use % as wildcard" name="notterm2" value="<?php echo $_SESSION['notterm2']; ?>" onKeyPress="if (event.keyCode == 13) pageforward(1);"/></td>
-			<td><br><input type="text" title="Tip: use % as wildcard" name="notterm3" value="<?php echo $_SESSION['notterm3']; ?>" onKeyPress="if (event.keyCode == 13) pageforward(1);"/></td>
-			<td><br><input type="text" title="Tip: use % as wildcard" name="notterm4" value="<?php echo $_SESSION['notterm4']; ?>" onKeyPress="if (event.keyCode == 13) pageforward(1);"/></td>
+			<td colspan="4">Exclude:</td>
 		</tr>
+		<tr>
+			<td><input type="text" title="Tip: use % as wildcard" name="notterm1" value="<?php echo $_SESSION['notterm1']; ?>" /></td>
+			<td><input type="text" title="Tip: use % as wildcard" name="notterm2" value="<?php echo $_SESSION['notterm2']; ?>" /></td>
+			<td><input type="text" title="Tip: use % as wildcard" name="notterm3" value="<?php echo $_SESSION['notterm3']; ?>" /></td>
+			<td><input type="text" title="Tip: use % as wildcard" name="notterm4" value="<?php echo $_SESSION['notterm4']; ?>" /></td>
+		</tr>
+		<tr>
+			<td colspan="2">Limit to strains frozen by:</td>
+			<td colspan="2">Show:</td>
+		</tr>
+		<tr>
+			<td><input type="text" name="sign1" value="<?php echo $_SESSION['sign1']; ?>" /></td>
+			<td>
+				<input type="text" name="limit" size=4 maxlength="5" value="<?php if(isset($_POST['limit'])) {echo $_POST['limit'];}else {echo 100;} ?>" />
+				<input type="hidden" name="startval" value="<?php if(isset($_POST['startval'])) {echo $_POST['startval'];}else {echo 1;} ?>" />
+				<input type="hidden" name="page"/>
+			</td>
+		</tr>
+	</table>
 
-
-	<?php
+<?php
 }
+
 //Are we in the 'List' tab?
 if($_GET['type'] == 'number' or $_GET['type'] == 'advanced') {
 	$helpmessage = "To list a range of strain numbers, use both of the input boxes. To find a single strain, use only one of the input boxes.<br>When you click 'Search', the values are remembered until you click 'Reset'."; ?>
-	List strain numbers:
+	<p>List strain numbers:</p>
 
     <table width="20" border="0">
-    	<tr><td colspan=4>Between:<br><input type="text" name="minNum" maxlength="5" value="<?php echo $_SESSION['minNum']; ?>" onKeyPress="if (event.keyCode == 13) pageforward(1); return number(event);"/></td><td></td></tr>
-    	<tr><td colspan=4>And:<br><input type="text" name="maxNum" maxlength="5" value="<?php echo $_SESSION['maxNum']; ?>" onKeyPress="if (event.keyCode == 13) pageforward(1); return number(event);"/></td></tr>
-     
+		<tr>
+			<td colspan="2">Between:</td>
+		</tr>
+    	<tr>
+    		<td colspan="2"><input type="text" name="minNum" maxlength="5" value="<?php echo $_SESSION['minNum']; ?>" /></td>
+    	</tr>
+		<tr>
+			<td colspan="2">And:</td>
+		</tr>
+    	<tr>
+    		<td colspan="2"><input type="text" name="maxNum" maxlength="5" value="<?php echo $_SESSION['maxNum']; ?>" /></td>
+    	</tr>
+		<tr>
+			<td>Limit to strains frozen by:</td>
+			<td>Show:</td>
+		</tr>
+		<tr>
+			<td><input type="text" name="sign1" value="<?php echo $_SESSION['sign1']; ?>" /></td>
+			<td>
+				<input type="text" name="limit" size=4 maxlength="5" value="<?php if(isset($_POST['limit'])) {echo $_POST['limit'];}else {echo 100;} ?>" />
+				<input type="hidden" name="startval" value="<?php if(isset($_POST['startval'])) {echo $_POST['startval'];}else {echo 1;} ?>" />
+				<input type="hidden" name="page"/>
+			</td>
+		</tr>
+	</table>
 
 <?php
 //**
 }
 ?>
 
-<tr><td colspan=2>Limit to strains frozen by:<br><input type="text" name="sign1" value="<?php echo $_SESSION['sign1']; ?>" onKeyPress="if (event.keyCode == 13) pageforward(1);"/></td>
-	<td>Show:<br>
-		<input type="text" name="limit" size=4 maxlength="5" value="<?php if(isset($_POST['limit'])) {echo $_POST['limit'];}else {echo 100;} ?>" onKeyPress="if (event.keyCode == 13) pageforward(1); return number(event);"/><br></td><td><input type="hidden" name="startval" value="<?php if(isset($_POST['startval'])) {echo $_POST['startval'];}else {echo 1;} ?>" /><input type="hidden" name="page"/><br>
-
-	</td>
-</tr>
-
-<tr>
-	<td width=20>
-	    <input type="hidden" name="isPosted" value="TRUE"/>
-	    <input type="button" name="button" value="Search" onClick="pageforward(1);"/></td><td>
-		<input type="button" title="Reset all fields and clear the results table" name="reset" value="Reset" onclick="window.location.href='index.php?mode=search&type=<?php echo $_GET['type'];?>&reset=TRUE'">
-	</td>
-</tr>
 </form>
-</table>
